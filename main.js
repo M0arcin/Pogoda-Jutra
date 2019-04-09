f02af67a09856915796037568ea6ec155a7c5d38
Konrad <ziomekja30@gmail.com>
2019-04-09 19:42:44 +0200

    Day and Night shift
    Plus lekkie komentarze

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var x = document.getElementById("city");
+
+//poniezej funkcja ktora podaje nam wspolrzedne lokalizacji uzytkownika po kliknieciu.
+var x = document.getElementById("city"); //tutaj moze je wyswietlic, city wpisane, ale nie bardzo wychodiz mi wstukanie ich do inputa.
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -10,5 +12,19 @@ function getLocation() {
 
 function showPosition(position) {
     let e=`${position.coords.latitude},${position.coords.longitude}`;
-    console.log(e);
+    console.log(e);//tu loguje kordy
 }
+//ta funkcja ustawia tlo w zaleznosci od pory dnia. poki co zmienia tlo forecasta, ale to szczegol do zmiany,
+function dayAndNight() {
+    var current = new Date();
+    var timer = current.getHours();
+    if (timer>6&&timer<=19) {
+          //Dzien
+          var bodyColor1 = document.getElementById("simple1");
+          bodyColor1.style.backgroundColor = "blue";
+    } else {
+          //Noc
+          var bodyColor2 = document.getElementById("simple1");
+          bodyColor2.style.backgroundColor = 'pink';
+    }
+} dayAndNight();
